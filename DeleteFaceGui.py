00a1ec83ae6c19c3850cb84c34f96c1253f746b4
Mai Xuan Vu <vucd3>
2021-03-17 16:21:08 +0700

    7th commit

--- DeleteFaceGui.py
+++ DeleteFaceGui.py
@@ -57,13 +57,6 @@ class DeleteFace(object):
         self.quit.setStyleSheet("background-color: red")
 
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
     
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
